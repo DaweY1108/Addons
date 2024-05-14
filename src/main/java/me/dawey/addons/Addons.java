519ce59b769b90bfe54d0d634da8ad9ae562a5b6
DaweY1108 <vargadavidzsolt@gmail.com>
2024-05-14 12:09:29 +0200

    Initial commit

---
+++ src/main/java/me/dawey/addons/Addons.java
@@ -0,0 +1,17 @@
+package me.dawey.addons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Addons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
